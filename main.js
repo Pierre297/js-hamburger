ef0d7277a8624731ca83974c7931d8d137f94aa9
Pier <pierfrancesco297@gmail.com>
2021-10-22 18:53:20 +0200

    active

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 // click hamburger menu
 const button = document.querySelector(".fa-bars");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+var hamburgerMenu = document.querySelector(".hamburger-menu");
 const buttonClose = document.querySelector(".fa-times");
 
+// rimozione active
+// document.querySelector(".hamburger-menu").classList.add("active");
+
+// bottone apertura
 button.addEventListener("click",
 
 function(){
 
     document.querySelector(".hamburger-menu").style.display = "unset";
-
-    var element = document.querySelector(".hamburger-menu");
-    element.classList.remove("active");
 }
-
-
 );
 
+// bottone chiusura
 buttonClose.addEventListener("click",
 
 function(){
 
     document.querySelector(".hamburger-menu").style.display = "none";
-
-        
+    
 }
-
-
 );
+
+
+
+
+
 
 
 
