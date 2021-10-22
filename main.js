06112de3bea6dbdc4766b915fe32cb30afbd2246
Pier <pierfrancesco297@gmail.com>
2021-10-22 16:02:24 +0200

    click hamburger menu

---
+++ main.js
@@ -0,0 +1,19 @@
+// click hamburger menu
+const button = document.getElementsByClassName("fa-bars");
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+
+button.addEventListener("click",
+
+function(){
+
+    document.getElementsByClassName("hamburger-menu").style.display = "unset";
+}
+
+
+);
+
+
+
+
+
+
