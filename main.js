82811cae33ab417e713003f24ad87625a6065733
Pier <pierfrancesco297@gmail.com>
2021-10-22 18:07:05 +0200

    prove class remove

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 // click hamburger menu
 const button = document.querySelector(".fa-bars");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+const buttonClose = document.querySelector(".fa-times");
 
 button.addEventListener("click",
 
 function(){
 
     document.querySelector(".hamburger-menu").style.display = "unset";
+
+    var element = document.querySelector(".hamburger-menu");
+    element.classList.remove("active");
+}
+
+
+);
+
+buttonClose.addEventListener("click",
+
+function(){
+
+    document.querySelector(".hamburger-menu").style.display = "none";
+
+        
 }
 
 
